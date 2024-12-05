7e0272d76a3da27af7836f5cfbfe6080a5cdef65
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-12-05 17:34:27 +0100

    [UPD] state management

---
+++ Code_teoria/4_state_management/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from './store';
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <div>{{ store.text }}</div>
+  <hr>
+  <input type="text" v-model="store.model">
+  <div v-if="store.model === ''">Hai scritto ...</div>
+  <div v-else>Hai scritto {{ store.model }}</div>
+  <hr>
+</template>
+
+<style scoped></style>
---
+++ Code_teoria/4_state_management/src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    text: 'Prova uno passaggio dati da Store.js',
+    model: ''
+})
